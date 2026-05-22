7c45de3e796b96f7560a7785308721bb66dc2103
akukuat12 <mdafa6327@gmail.com>
2026-05-22 14:37:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,6 @@ import pandas as pd
 import streamlit as st
 from numpy.random import default_rng as rng
 
-df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
-chart = (
-    alt.Chart(df)
-    .mark_circle()
-    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
-)
-
 st.write(chart)
 import streamlit as st
 
