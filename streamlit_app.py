0b7d99d5019b4587e74aabd2e9f7e327403945d0
akukuat12 <mdafa6327@gmail.com>
2026-05-22 14:20:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 RAMDAN GANTENG")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+with st.container():
+    st.write("Text inside the container")
+    st.button("A button inside the container")
