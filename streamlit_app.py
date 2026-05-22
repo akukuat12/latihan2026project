a35e8247d68d8a8661db03362bfab4ca01a0a6cd
akukuat12 <mdafa6327@gmail.com>
2026-05-22 14:22:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
 st.title("🎈 RAMDAN GANTENG")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import altair as alt
+import pandas as pd
 import streamlit as st
+from numpy.random import default_rng as rng
 
-with st.container():
-    st.write("Text inside the container")
-    st.button("A button inside the container")
-streamlit run your_script.py
+df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.write(chart)
